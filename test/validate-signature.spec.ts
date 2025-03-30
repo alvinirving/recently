9ae92c1ecca7e707c77338c5da8e6e3ba24a687b
alvinirving <alvinirving1@yahoo.com>
2025-03-30 21:04:09 +0000

    Add README - fixing typo

--- test/validate-signature.spec.ts
+++ test/validate-signature.spec.ts
@@ -3,17 +3,17 @@ import validateSignature from "../lib/validate-signature.js";
 
 import { describe, it } from "vitest";
 
-const body = { hello: "world" };
-const secret = "test_secret";
+let body = { hello: "world" };
+let secret = "test_secret";
 
 describe("validateSignature", () => {
   it("success", () => {
-    const validSignature = "t7Hn4ZDHqs6e+wdvI5TyQIvzie0DmMUmuXEBqyyE/tM=";
+    let validSignature = "t7Hn4ZDHqs6e+wdvI5TyQIvzie0DmMUmuXEBqyyE/tM=";
     ok(validateSignature(JSON.stringify(body), secret, validSignature));
   });
 
   it("failure", () => {
-    const invalidSignature = "t7Hn4ZDHqs6e+wdvi5TyQivzie0DmMUmuXEBqyyE/tM=";
+    let invalidSignature = "t7Hn4ZDHqs6e+wdvi5TyQivzie0DmMUmuXEBqyyE/tM=";
     ok(!validateSignature(JSON.stringify(body), secret, invalidSignature));
   });
 });
